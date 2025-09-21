6dfbc03d744b3637ce801ac419e79401e90e85d6
Lucas Furlan <155035762+furlanflucas@users.noreply.github.com>
2025-09-21 19:30:05 +0000

    debugging

--- portfolio-particles/next.config.mjs
+++ portfolio-particles/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enables static export
+  output: 'export',
+  images: {
+    unoptimized: true, // Needed for static export
+  }
 };
 
 export default nextConfig;
